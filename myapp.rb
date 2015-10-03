0022b4f154c1c13055055f554e40c384077012d6
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-10-03 11:33:00 -0400

    adding numbs in url

--- myapp.rb
+++ myapp.rb
@@ -3,3 +3,19 @@ require 'sinatra.rb'
 get '/' do 
 'My name is Joseph Mckenzie'
 end
+
+get '/joe' do
+"My dogs name was bubba"
+end
+
+get '/hello/:name' do |n|
+
+"Hello #{n}!"
+end
+
+get "/add/:int1/:int2/:int3" do |a,b,c|
+
+"#{a.to_i + b.to_i*c.to_i}"
+
+
+end
